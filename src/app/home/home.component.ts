93f9e4b75db10e2a0c3b70cdb52b836ed0eeb3cd
Diogo Santos <diogomariasantos3@gmail.com>
2024-03-08 09:18:56 +0000

    Created Arrays for buttons info

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 @Component({
   selector: 'home-page',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  
+  buttonsFirstLine: homeButtons[] = [
+    {
+      img: '../../assets/home_page_icons/properties.png',
+      imgTitle: 'Properties'
+    },
+    {
+      img: '../../assets/home_page_icons/sellers.png',
+      imgTitle: 'Sellers'
+    },
+    {
+      img: '../../assets/home_page_icons/customers.png',
+      imgTitle: 'Customers'
+    }
+  ]
+
+  buttonsSecondLine: homeButtons[] = [
+    {
+      img: '../../assets/home_page_icons/visit.png',
+      imgTitle: 'Visit'
+    },
+    {
+      img: '../../assets/home_page_icons/reference.png',
+      imgTitle: 'Reference'
+    },
+    {
+      img: '../../assets/home_page_icons/options.png',
+      imgTitle:'Options'
+    }
+  ]
+}
+
+interface homeButtons {
+  img: string,
+  imgTitle: string
 }
