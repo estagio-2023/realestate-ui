3102bb476f2a7556a5d409ab366b6942e2b65836
Rafael Meha <97791234+RafaelMeha@users.noreply.github.com>
2024-04-11 14:16:21 +0100

    Create a new component "customer-management-component", and added the route in the "app.route.ts" file.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PropertyManagementComponent } from './property-management/property-management.component';
 import { ReferenceDataComponent } from './reference-data/reference-data.component';
+import { CustomerManagementComponent } from './customer-management/customer-management.component';
 
 const routerConfig: Routes = [
     {
@@ -18,6 +19,11 @@ const routerConfig: Routes = [
         path: 'reference-data',
         component: ReferenceDataComponent,
         data: {title: 'Reference Data'}
+    },
+    {
+        path: 'customer',
+        component: CustomerManagementComponent,
+        data: {title: 'Customer Management'}
     }
 ];
 
---
+++ src/app/customer-management/customer-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './customer-management.component.html',
+  styleUrl: './customer-management.component.css'
+})
+export class CustomerManagementComponent {
+
+}
