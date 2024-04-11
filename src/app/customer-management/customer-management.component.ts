112eb321db76990d6a1c33dc623183c9f971b872
Rafael Meha <97791234+RafaelMeha@users.noreply.github.com>
2024-04-11 17:06:31 +0100

    removed all css

--- src/app/customer-management/customer-management.component.ts
+++ src/app/customer-management/customer-management.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-customer-management',
-  standalone: true,
-  imports: [],
   templateUrl: './customer-management.component.html',
   styleUrl: './customer-management.component.css'
 })
