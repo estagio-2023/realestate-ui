c77b1bcbdafe81ca6548c38d22a6fdc6ad8b6734
Diogo Santos <diogomariasantos3@gmail.com>
2024-04-30 15:12:04 +0100

    Fetch all property management data

--- src/app/property-management/property-management.component.ts
+++ src/app/property-management/property-management.component.ts
@@ -1,19 +1,29 @@
 import { Component } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { PropertyManagementModalComponent } from '../modals/property-management-modal/property-management-modal.component';
+import { PropertyManagementApiService } from '../services/property-management-api.service';
+import { PropertyHeader, PropertyBody } from '../models/property-management-model';
 
 @Component({
   selector: 'app-property-management',
   templateUrl: './property-management.component.html',
   styleUrl: './property-management.component.css'
 })
-export class PropertyManagementComponent {
+export class PropertyManagementComponent{
+  propertiesHeaderList: PropertyHeader[]
+  propertyBody: PropertyBody | undefined
 
-  constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal, private apiService: PropertyManagementApiService){}
 
-  openModal() {
+  ngOnInit(): void {
+    this.apiService.getAllProperties().subscribe(response => {
+      this.propertiesHeaderList = response
+    })
+  }
+
+  openModal(){
     this.modalService.open(PropertyManagementModalComponent, {
       keyboard: false
-    });
+    })
   }
 }
