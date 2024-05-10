b3f4f5c402f1196d008e79bf0edcedb359003a52
evaalves2042 <evaalves6782@gmail.com>
2024-05-10 15:47:56 +0100

    correção de código

---
+++ src/app/view-customer/view-customer.component.ts
@@ -0,0 +1,19 @@
+//import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-view-customer',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-customer.component.html',
+  styleUrl: './view-customer.component.css'
+})
+
+export class ViewCustomerComponent {
+  @Input() name = ''; 
+  @Input() password = '';
+  @Input() email = '';
+}
+
