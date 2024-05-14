762ee00d0b2afd0f3621d69818a5de910b458de2
Rafael Meha <97791234+RafaelMeha@users.noreply.github.com>
2024-05-14 10:57:03 +0100

    Added pipe async method in the real estate component

--- src/app/real-estate-management/real-estate-management.component.ts
+++ src/app/real-estate-management/real-estate-management.component.ts
@@ -3,22 +3,22 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { RealEstateManagementModalComponent } from '../modals/real-estate-management-modal/real-estate-management-modal.component';
 import { RealEstateManagementApiService } from '../services/real-estate-management-api.service';
 import { RealEstateHeader, RealEstateBody } from '../models/real-estate-management-model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-real-estate-management',
   templateUrl: './real-estate-management.component.html',
   styleUrl: './real-estate-management.component.css'
 })
+
 export class RealEstateManagementComponent{
-  realEstatesHeaderList: RealEstateHeader[]
+  realEstatesHeaderList$: Observable<RealEstateHeader[]>
   realEstateBody: RealEstateBody | undefined
 
   constructor(private modalService: NgbModal, private apiService: RealEstateManagementApiService){}
 
   ngOnInit(): void {
-    this.apiService.getAllRealEstates().subscribe(response => {
-      this.realEstatesHeaderList = response
-    })
+    this.realEstatesHeaderList$ = this.apiService.getAllRealEstates();
   }
 
   getRealEstateBody(realEstateId: number) {
@@ -27,9 +27,19 @@ export class RealEstateManagementComponent{
     })
   }
 
+  loadRealEstateData(){
+    this.realEstatesHeaderList$ = this.apiService.getAllRealEstates();
+  }
+
   openModal(){
-    this.modalService.open(RealEstateManagementModalComponent, {
+    var response = this.modalService.open(RealEstateManagementModalComponent, {
       keyboard: false
+    })
+
+    response.result.then((data) => {
+      if (data != null) {
+        this.loadRealEstateData();
+      }
     })
   }
 }
