c634ac3a4695c2e4e3654a50452cb3350dad7af0
Jonathan Osório <jonathan.d.osorio@cgi.com>
2024-05-15 14:52:12 +0100

    Created UI for agent management
    - Generated Component
    - Specified Routes
    - Inserted Bootstrap properties in html

---
+++ src/app/agent-management/agent-management/agent-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agent-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './agent-management.component.html',
+  styleUrl: './agent-management.component.css'
+})
+export class AgentManagementComponent {
+
+}
