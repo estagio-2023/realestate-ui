eb2962790c1d64de95b9b3833d2b24c1ab757a19
Jonathan Osório <jonathan.d.osorio@cgi.com>
2024-05-16 10:14:44 +0100

    html for agent view

---
+++ src/app/views/agent-view/agent-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agent-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './agent-view.component.html',
+  styleUrl: './agent-view.component.css'
+})
+export class AgentViewComponent {
+
+}
