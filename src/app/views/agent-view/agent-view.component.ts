3a49783bed154eced1ac2182c06a056ad03dd1ce
Jonathan Osório <jonathan.d.osorio@cgi.com>
2024-05-17 17:02:00 +0100

    Implemented Get Method for the view agent page

--- src/app/views/agent-view/agent-view.component.ts
+++ src/app/views/agent-view/agent-view.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AgentService } from '../../services/agent.service';
+import { AgentModel } from '../../models/agent-model';
 
 @Component({
   selector: 'app-agent-view',
@@ -10,14 +12,18 @@ export class AgentViewComponent implements OnInit {
     @Input() name = ''; 
     @Input() phoneNumber = '';
     @Input() email = '';
+    agentData: AgentModel | undefined
+    agentId: number;
   
-    constructor(private activatedRoute: ActivatedRoute){}
-  
+    constructor(private activatedRoute: ActivatedRoute, private apiService: AgentService){}
+
     ngOnInit(): void {
       this.activatedRoute.params.subscribe(params => {
-        let id = params['id'];
-      
-        console.log(`${id}`);
-        });
+        this.agentId = params['id'];
+      });
+      this.apiService.getAgentById(this.agentId).subscribe(response => {
+        this.agentData = response
+      });
     }
+    
 }
