dc4725e68e4ca629d2d789f9360402f7719e6b6c
evaalves2042 <evaalves6782@gmail.com>
2024-05-17 13:56:07 +0100

    botão view funciona, falta o html estar correto e os dados

--- src/app/view-customer/view-customer.component.ts
+++ src/app/view-customer/view-customer.component.ts
@@ -1,13 +1,9 @@
 //import { Component } from '@angular/core';
 import { Component, Input, OnInit} from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
-
 
 @Component({
   selector: 'app-view-customer',
-  //standalone: true,
-  //imports: [],
   templateUrl: './view-customer.component.html',
   styleUrl: './view-customer.component.css'
 })
