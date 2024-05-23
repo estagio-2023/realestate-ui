19868a23722da6f16e1237b7d284aeaa040536a1
Rafael Meha <rafaelmeha5@gmail.com>
2024-05-23 10:12:18 +0100

    Added async for edit method in agent component

--- src/app/views/agent-view/agent-view.component.ts
+++ src/app/views/agent-view/agent-view.component.ts
@@ -13,58 +13,68 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-agent-view',
   templateUrl: './agent-view.component.html',
-  styleUrl: './agent-view.component.css'
+  styleUrls: ['./agent-view.component.css']
 })
 export class AgentViewComponent implements OnInit {
-    @Input() name = ''; 
-    @Input() phoneNumber = '';
-    @Input() email = '';
-    agentData$: Observable<AgentModel>
-    agentData: AgentModel
-    agentId: number;
-    form = agentForm
-  
-    constructor(private activatedRoute: ActivatedRoute, private apiService: AgentService, private modalService: NgbModal, private toastService: ToastService){}
+  @Input() name = ''; 
+  @Input() phoneNumber = '';
+  @Input() email = '';
+  agentData$: Observable<AgentModel>;
+  agentData: AgentModel = {} as AgentModel;  
+  agentId: number;
+  form = agentForm;
 
-    ngOnInit(): void {
-      this.activatedRoute.params.subscribe(params => {
-        this.agentId = params['id'];
-      });
-      this.apiService.getAgentById(this.agentId).subscribe(response => {
-        this.agentData = response
-      });
-    }   
-    
-    deleteModal(agentId: number){
-      var response = this.modalService.open(DeleteAgentValidationModalComponent, {
-        keyboard: false
-      })
-      response.componentInstance.agentId = agentId;
-    }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private apiService: AgentService,
+    private modalService: NgbModal,
+    private toastService: ToastService
+  ) {}
 
-    openEditModal(agentData: AgentModel){
-      var response = this.modalService.open(AgentModalComponent, {
-        keyboard: false
-      })
-      response.componentInstance.agentData = agentData;
-      
-      response.result.then((data) => {
-        if(data === 'save'){
-          this.editAgent()
-        }
-      });
-    }
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.agentId = params['id'];
+      this.loadAgentData();
+    });
+  }
 
-    editAgent() {
-      this.apiService.editAgent(this.agentId, this.form.value).subscribe({
-        next: value => {
-          this.toastService.show("Agent edited successfully!", ToastClassEnum.success);
-          this.form.reset()
-        },
-        error: err => {
-          this.toastService.show("Error in editing agent", ToastClassEnum.error);
-          console.error(err);
-        }
-      });
-    }
+  loadAgentData(): void {
+    this.apiService.getAgentById(this.agentId).subscribe(response => {
+      this.agentData = response;
+    });
+  }
+
+  deleteModal(agentId: number): void {
+    const response = this.modalService.open(DeleteAgentValidationModalComponent, {
+      keyboard: false
+    });
+    response.componentInstance.agentId = agentId;
+  }
+
+  openEditModal(agentData: AgentModel): void {
+    const response = this.modalService.open(AgentModalComponent, {
+      keyboard: false
+    });
+    response.componentInstance.agentData = agentData;
+
+    response.result.then((data) => {
+      if (data === 'save') {
+        this.editAgent();
+      }
+    });
+  }
+
+  editAgent(): void {
+    this.apiService.editAgent(this.agentId, this.form.value).subscribe({
+      next: value => {
+        this.toastService.show('Agent edited successfully!', ToastClassEnum.success);
+        this.form.reset();
+        this.loadAgentData();
+      },
+      error: err => {
+        this.toastService.show('Error in editing agent', ToastClassEnum.error);
+        console.error(err);
+      }
+    });
+  }
 }
