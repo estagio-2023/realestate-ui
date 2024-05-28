a36111924cc294fe2ca603afcaec3812b2fff34b
pmzpro <pedromcruz97@gmail.com>
2024-05-28 16:50:37 +0100

    Fixed startTime and endTime fields

--- src/app/visit-request/components/modals/visit-request-management-modal/visit-request-management-modal.component.ts
+++ src/app/visit-request/components/modals/visit-request-management-modal/visit-request-management-modal.component.ts
@@ -18,24 +18,41 @@ export class VisitRequestManagementModalComponent {
   minDate: string
   agents : AgentModel[]
   times: string[] = []
+  endTimes: string[] = []
+  filteredEndTimes: string[] = []
   startTime: string = ''
   endTime: string = ''
+  date: string
   
   constructor(public activeModal: NgbActiveModal,private agentService: AgentService, private visitRequestService: VisitRequestService, private toastService: ToastService){
     const today = new Date();
     this.minDate = today.toISOString().split('T')[0]
     
-    for (let hour = 9; hour <= 20; hour++) {
+    for (let hour = 9; hour < 20; hour++) {
       const timeString = hour < 10 ? `0${hour}:00` : `${hour}:00`;
-      this.times.push(timeString);
+      this.times.push(timeString)
+      this.endTimes.push(timeString)
     }
+    this.endTimes.push('20:00')
   }
 
   ngOnInit(): void{
     this.agentService.getAllAgentData().subscribe(response => {
       this.agents = response.filter(agents => agents.name)
   });
+
+  this.filteredEndTimes = [...this.times];
   }
+
+  updateEndTimes() {
+    this.filteredEndTimes = this.endTimes.filter(endTime => endTime > this.startTime);
+    this.endTimes = this.filteredEndTimes
+}
+
+  changeDate(date: any){
+  this.date = date
+    console.log(this.date)
+    }
 
   closeModal(result: any) {
     this.activeModal.close(result)
