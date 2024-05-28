1cae43574c9e24ac41892a50b39f1beb5ba408e9
Rafael Meha <97791234+RafaelMeha@users.noreply.github.com>
2024-05-28 09:57:08 +0100

    fixed email validator

--- src/app/common/services/form/form.service.ts
+++ src/app/common/services/form/form.service.ts
@@ -32,6 +32,6 @@ export const realEstateForm = new FormGroup({
 
 export const agentForm = new FormGroup({
   name: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(100)]),
-  email: new FormControl('', [Validators.required, Validators.email, Validators.minLength(9), Validators.maxLength(13)]),
-  phoneNumber: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(150)]),
+  email: new FormControl('', [Validators.required, Validators.email, Validators.minLength(8), Validators.maxLength(150)]),
+  phoneNumber: new FormControl('', [Validators.required, Validators.minLength(9), Validators.maxLength(13)]),
 });
